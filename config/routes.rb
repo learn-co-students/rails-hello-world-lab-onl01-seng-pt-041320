a20f4328388421cc99e79bc5063ee0cd70ac36da
Dice-K39 <daisukehashimoto39@gmail.com>
2020-08-09 08:26:48 -0400

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   # either works
   get '/hello_world' => 'hello_world#hello_world'
-  get '/hello_world', to: 'hello_world#hello_world'
+  #get '/hello_world', to: 'hello_world#hello_world'
 end
